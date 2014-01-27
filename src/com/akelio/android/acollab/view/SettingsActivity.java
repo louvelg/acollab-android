8a73d7d81fe6ff100a65958cabf2fe3e8b30a60b
louvelg <louvel@akelio.fr>
2014-01-27 23:09:52 +0100

    add settings activity

---
+++ src/com/akelio/android/acollab/view/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.akelio.android.acollab.view;
+
+import com.akelio.android.acollab.R;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+
+}
