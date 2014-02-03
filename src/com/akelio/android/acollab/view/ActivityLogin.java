ae8b962e475e74a2a26fb6087f671b85b3418373
louvelg <louvel@akelio.fr>
2014-02-03 18:16:12 +0100

    update login Activity to save login and password and to go to activityStream Activity automaticcally if login and password are set

--- src/com/akelio/android/acollab/view/ActivityLogin.java
+++ src/com/akelio/android/acollab/view/ActivityLogin.java
@@ -1,108 +1,68 @@
 package com.akelio.android.acollab.view;
 
-import org.springframework.http.HttpAuthentication;
-import org.springframework.http.HttpBasicAuthentication;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.StringUtils;
 import android.app.Activity;
-import android.os.AsyncTask;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import com.akelio.android.acollab.R;
 
+public class ActivityLogin extends Activity implements android.view.View.OnClickListener {
 
-public class ActivityLogin extends Activity implements android.view.View.OnClickListener{
+	EditText	login;
+	EditText	password;
+	Button		buttonSend;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (hasLoginInfo()) {
+			startActivity(new Intent(ActivityLogin.this, ActivityStreamActivity.class));
+			return;
+		}
+
 		setContentView(R.layout.activity_login);
-		
-		final Button buttonSend = (Button) findViewById(R.id.buttonConnexion);
+
+		login = (EditText) findViewById(R.id.editTextLogin);
+		password = (EditText) findViewById(R.id.editTextPasswd);
+		buttonSend = (Button) findViewById(R.id.buttonLogin);
 		buttonSend.setOnClickListener(this);
-		
-		//final EditText editLogin = (EditText) findViewById(R.id.editTextLogin);
-		//final EditText editPasswd = (EditText) findViewById(R.id.editTextPasswd);
+
 	}
-	private class LoadWebPageASYNC extends AsyncTask<String, Void, String> {
 
-		@Override
-		protected String doInBackground(String... urls) {
-			String url = "https://ajax.googleapis.com/ajax/" + "services/search/web?v=1.0&q={query}";
-			System.out.println("url : " + url);
-
-			try {
-				// Create a new RestTemplate instance
-				RestTemplate restTemplate = new RestTemplate();
-				restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-				String result = restTemplate.getForObject(url, String.class, "Android");
-				System.out.println("url : " + result);
-
-				url = "http://geb.test1.acollab.com/rest/v1/login";
-
-				HttpAuthentication authHeader = new HttpBasicAuthentication("admin", "admin");
-				HttpHeaders requestHeaders = new HttpHeaders();
-				requestHeaders.setAuthorization(authHeader);
-				HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
-
-				restTemplate = new RestTemplate();
-				restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-				ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
-				System.out.println("url : " + response.getBody());
-
-				restTemplate = new RestTemplate();
-				restTemplate.getMessageConverters().add(new GsonHttpMessageConverter());
-				ResponseEntity<Login> response2 = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Login.class);
-				System.out.println("url : " + response2.getBody().getTenantId());
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-
-		}
-	}
-	
-	public class Login {
-		private String	tenantId;
-		private String	userId;
-
-		public String getTenantId() {
-			return tenantId;
-		}
-
-		public void setTenantId(String tenantId) {
-			this.tenantId = tenantId;
-		}
-
-		public String getUserId() {
-			return userId;
-		}
-
-		public void setUserId(String userId) {
-			this.userId = userId;
-		}
-	}
-	
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
 		switch (v.getId()) {
-			case R.id.buttonConnexion:
-				System.out.println("Start buttonLogin");
-				LoadWebPageASYNC task = new LoadWebPageASYNC();
-				task.execute(new String[] { "http://www.javacodegeeks.com" });
+			case R.id.buttonLogin:
+				login(v);
+
 				break;
 		}
+	}
+
+	private boolean hasLoginInfo() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		if (StringUtils.hasText(prefs.getString("login", null)) && StringUtils.hasText(prefs.getString("password", null)) && StringUtils.hasText(prefs.getString("url", null))) { return true; }
+		return false;
+	}
+
+	private void login(View v) {
+		String l = login.getText().toString();
+		String p = password.getText().toString();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		Editor editor = prefs.edit();
+		if (StringUtils.hasText(l)) {
+			editor.putString("login", l);
+		}
+		if (StringUtils.hasText(p)) {
+			editor.putString("password", p);
+		}
+		editor.commit();
+		startActivity(new Intent(ActivityLogin.this, ActivityStreamActivity.class));
 	}
 }
